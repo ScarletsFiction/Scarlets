186ddc85b974f4ae3a9af649f4538003cb7bc62d
StefansArya <arya.cipta@yahoo.com>
2018-08-07 14:38:54 +0700

    Fix mobile responsive viewport

--- example/resources/views/static/header.php
+++ example/resources/views/static/header.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html><html>
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="theme-color" content="#181818">
 	<title><?= $title ?></title>
 	<link rel="stylesheet" type="text/css" href="<?= $q::$home ?>/assets/css/style.css">
 	<link href='<?= $q::$home ?>/assets/css/lato-font.css' rel='stylesheet' type='text/css'>
