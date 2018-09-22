423ee79ea68c48e37b0c2472ba13aa68474725b9
StefansArya <arya.cipta@yahoo.com>
2018-09-22 07:30:16 +0700

    Fix bugs and add feature to get duplicated array value

--- src/Extend/Arrays.php
+++ src/Extend/Arrays.php
@@ -7,11 +7,22 @@ class Arrays{
 		$sort = $ascending ? SORT_ASC : SORT_DESC;
 		$temp = [];
 		$temp_ = &$array;
-		foreach ($temp_ as $key => $row) {
-   			if($caseinsensitive) $temp[$key] = strtolower($row[$part]);
-   			else $temp[$key] = &$row[$part];
+		foreach ($temp_ as $part => $row) {
+   			if($caseinsensitive) $temp[$part] = strtolower($row[$key]);
+   			else $temp[$part] = &$row[$key];
 		}
 		array_multisort($temp, $sort, $temp_);
 		return $temp_;
+	}
+	
+	public static function &duplicates($array)
+	{
+    	$duplicates = [];
+    	$values = array_count_values($array);
+    	foreach ($values as $value => $count) {
+    		if($count > 1) $duplicates[] = $value;
+    	}
+
+    	return $duplicates;
 	}
 }
