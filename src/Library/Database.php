e73cbcd90c6b145a1017382876b51d560bfe2256
StefansArya <arya.cipta@yahoo.com>
2018-09-23 01:18:17 +0700

    Improve performance by using shared database connection

--- src/Library/Database.php
+++ src/Library/Database.php
@@ -18,16 +18,48 @@ class Database{
 		(credential) CredentialID that configured on the configuration
 	*/
 	public static function &connect($credential=false){
+		// Use default credential if not specified
 		if($credential === false)
 			$credential = self::$default;
 
-		// Check if connected
+		// Return the connected database
 		if(isset(self::$connectedDB[$credential]))
 			return self::$connectedDB[$credential];
-		else{
-			self::$connectedDB[$credential] = new SQL(self::$credentials[$credential]);
-			return self::$connectedDB[$credential];
+
+		// Get reference of requested database credential
+		$ref = &self::$credentials[$credential];
+		$copy = false;
+
+		// Check if it's referenced to other credential
+		if(isset(self::$credentials[$credential]['connection'])){
+			$requestedCredential = $credential;
+			$credential = $ref['connection'];
+
+			// Try to obtain copy after connected
+			$copy = true;
 		}
+
+		// Connect if not connected
+		if(!isset(self::$connectedDB[$credential])){
+			$options = &self::$credentials[$credential];
+			$driver = &$options['driver'];
+
+			// MySQL
+			if($driver === 'mysql')
+				self::$connectedDB[$credential] = new SQL($options);
+			// Else ...
+		}
+		$db = &self::$connectedDB[$credential];
+
+		// Obtain the clone
+		if($copy){
+			$copy = clone $db;
+			$copy->change($ref);
+			self::$connectedDB[$requestedCredential] = &$copy;
+			return $copy;
+		}
+
+		return $db;
 	}
 
 	public static function init(){
