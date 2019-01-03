5cd10b5b90f254dba3bd920920c8e92529bfe24a
StefansArya <arya.cipta@yahoo.com>
2019-01-03 21:24:29 +0700

    Add addional feature for array manipulation

--- src/Extend/Arrays.php
+++ src/Extend/Arrays.php
@@ -2,8 +2,7 @@
 namespace Scarlets\Extend;
 
 class Arrays{
-	public static function &sortByKey($array, $key, $ascending=true, $caseinsensitive=false)
-	{
+	public static function &sortByKey($array, $key, $ascending = true, $caseinsensitive = false){
 		$sort = $ascending ? SORT_ASC : SORT_DESC;
 		$temp = [];
 		$temp_ = &$array;
@@ -15,8 +14,7 @@ class Arrays{
 		return $temp_;
 	}
 	
-	public static function &duplicates($array)
-	{
+	public static function &duplicates($array){
     	$duplicates = [];
     	$values = array_count_values($array);
     	foreach ($values as $value => $count) {
@@ -24,5 +22,34 @@ class Arrays{
     	}
 
     	return $duplicates;
+	}
+
+	public static function uniqueDifference($array1, $array2){
+		$array1 = array_flip($array1);
+		$array2 = array_flip($array2);
+
+		$new = [];
+		$unchanged = [];
+		foreach ($array2 as $key => $value) {
+			if(isset($array1[$key])){
+				unset($array1[$key]);
+				$unchanged[] = $key;
+			}
+			else $new[] = $key;
+		}
+
+		return [
+			'new'=>$new,
+			'unchanged'=>$unchanged,
+			'deleted'=>array_flip($array1),
+		];
+	}
+
+	public static function contains($str, $arr){
+	    foreach($arr as &$a) {
+	        if (stripos($str, $a) !== false)
+	        	return true;
+	    }
+	    return false;
 	}
 }
