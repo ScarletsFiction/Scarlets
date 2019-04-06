c8208ed65b9ae067e37a000df4d1f965475e3fc5
StefansArya <arya.cipta@yahoo.com>
2019-04-06 15:41:45 +0700

    Improve WebRequest for supporting parallel request and multiple download

--- src/Library/WebRequest.php
+++ src/Library/WebRequest.php
@@ -3,16 +3,24 @@ namespace Scarlets\Library;
 use \Scarlets;
 
 class WebRequest{
+	public static $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36';
 	public static function loadURL($url, $options = false){
-		$userAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36';
 		$headers = ['Accept'=>'*/*;q=0.8'];
-		
-		$ch = curl_init();
+		$options_ = [
+			CURLOPT_ENCODING=>'gzip',
+			CURLOPT_USERAGENT=>self::$userAgent,
+			CURLOPT_RETURNTRANSFER=>1,
+			CURLOPT_FOLLOWLOCATION=>1,
+			CURLOPT_MAXREDIRS=>5,
+			CURLOPT_AUTOREFERER=>1,
+			CURLOPT_SSL_VERIFYHOST=>0,
+			CURLOPT_SSL_VERIFYPEER=>0
+		];
 		
 		if($options){
 			if(isset($options['ssl'])){ # 0, 1
-				curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, $options['ssl']);
-				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $options['ssl']);
+				$options_[CURLOPT_SSL_VERIFYHOST] = $options['ssl'];
+				$options_[CURLOPT_SSL_VERIFYPEER] = $options['ssl'];
 			}
 
 			if(isset($options['header'])) # ['X-Custom'=>'values']
@@ -27,26 +35,26 @@ class WebRequest{
 						$url .= '?'.http_build_query($options['data']);
 
 					elseif($options['method'] === 'POST'){
-						curl_setopt($ch, CURLOPT_POST, 1);
-						curl_setopt($ch, CURLOPT_POSTFIELDS, $options['data']);
+						$options_[CURLOPT_POST] = 1;
+						$options_[CURLOPT_POSTFIELDS] = $options['data'];
 					}
 
     				// Encode the data as JSON and send on the body
 					else {
 						if($options['method'] === 'JSON_POST')
-							curl_setopt($ch, CURLOPT_POST, 1);
+							$options_[CURLOPT_POST] = 1;
 
 						$headers['Content-Type'] = 'application/json; charset=utf-8';
-						curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($options['data']));
+						$options_[CURLOPT_POSTFIELDS] = json_encode($options['data']);
 					}
 
-					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $options['method']);
+					$options_[CURLOPT_CUSTOMREQUEST] = $options['method'];
 				}
 
 				elseif($options['method'] === 'HEAD')
-					curl_setopt($ch, CURLOPT_NOBODY, 1);
+					$options_[CURLOPT_NOBODY] = 1;
 
-    			else curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $options['method']);
+    			else $options_[CURLOPT_CUSTOMREQUEST] = $options['method'];
     		}
 
     		// Default GET parameter
@@ -58,95 +66,109 @@ class WebRequest{
 				if(!file_exists($options['cookiefile']))
 					file_put_contents($options['cookiefile'], '');
 
-				curl_setopt($ch, CURLOPT_COOKIE, file_get_contents($options['cookiefile']));
+				$options_[CURLOPT_COOKIE] = file_get_contents($options['cookiefile']);
 			}
 
 			if(isset($options['cookie'])) # 'field=value;data=values'
-				curl_setopt($ch, CURLOPT_COOKIE, $options['cookie']);
+				$options_[CURLOPT_COOKIE] = $options['cookie'];
 
 			if(isset($options['limitSize'])){ # in KB
-				curl_setopt($ch, CURLOPT_BUFFERSIZE, 128); // more progress info
-				curl_setopt($ch, CURLOPT_NOPROGRESS, false);
-				curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, function(
+				$options_[CURLOPT_BUFFERSIZE] = 128; // more progress info
+				$options_[CURLOPT_NOPROGRESS] = false;
+				$options_[CURLOPT_PROGRESSFUNCTION] = function(
 					$DownloadSize, $Downloaded, $UploadSize, $Uploaded
 				) use($options) {
 					// If $Downloaded exceeds 1KB, returning non-0 breaks the connection!
 					return ($Downloaded > ($options['limitSize'] * 1024)) ? 1 : 0;
-				});
+				};
 			}
 
 			if(isset($options['proxy'])){ # [ip=>127.0.0.1, port=>3000, *userpass=>'user:pass']
-				curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_NTLM);
-				curl_setopt($ch, CURLOPT_PROXY, $options['proxy']['ip']);
-				curl_setopt($ch, CURLOPT_PROXYPORT, $options['proxy']['port']);   
+				$options_[CURLOPT_PROXYAUTH] = CURLAUTH_NTLM;
+				$options_[CURLOPT_PROXY] = $options['proxy']['ip'];
+				$options_[CURLOPT_PROXYPORT] = $options['proxy']['port'];   
 
 				if(isset($options['userpass']))
-					curl_setopt($ch, CURLOPT_PROXYUSERPWD, $options['proxy']['userpass']);
+					$options_[CURLOPT_PROXYUSERPWD] = $options['proxy']['userpass'];
 			}
 
 			if(isset($options['returnheader']) || isset($options['cookiefile']))
-				curl_setopt($ch, CURLOPT_HEADER, 1);
+				$options_[CURLOPT_HEADER] = 1;
 		}
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
-		curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
-		curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
 
 		$header = [];
 		foreach ($headers as $key => $value) {
 			$header[] = "$key: $value";
 		}
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		$options_[CURLOPT_HTTPHEADER] = $header;
 
-		$result = curl_exec($ch);
-		if($options){
-			if(isset($options['utf8']))
-				$result = mb_convert_encoding($result, 'utf-8', 'shift-jis');
+		$preprocess = function($result, &$ch) use(&$options) {
+			if($options){
+				if(isset($options['utf8']))
+					$result = mb_convert_encoding($result, 'utf-8', 'shift-jis');
 
-			if(isset($options['returnheader']) || isset($options['cookiefile'])){
-				$err = curl_errno($ch);
-				$errmsg = curl_error($ch);
-				$header = curl_getinfo($ch);
-				curl_close($ch);
+				if(isset($options['returnheader']) || isset($options['cookiefile'])){
+					$err = curl_errno($ch);
+					$errmsg = curl_error($ch);
+					$header = curl_getinfo($ch);
+					curl_close($ch);
 
-				$http_code = $header['http_code'];
-				$body = substr($result, $header['header_size']);
-				$header = self::parseHTTPHeader(substr($result, 0, $header['header_size']));
+					$http_code = $header['http_code'];
+					$body = substr($result, $header['header_size']);
+					$header = self::parseHTTPHeader(substr($result, 0, $header['header_size']));
 
-				if(isset($header['Set-Cookie'])){
-					if(is_array($header['Set-Cookie']))
-						$cookies = implode('; ', $header['Set-Cookie']);
-					else $cookies = $header['Set-Cookie'];
+					if(isset($header['Set-Cookie'])){
+						if(is_array($header['Set-Cookie']))
+							$cookies = implode('; ', $header['Set-Cookie']);
+						else $cookies = $header['Set-Cookie'];
+					}
+					else $cookies = false;
+					
+					if(isset($options['cookiefile']) && $cookies !== false){
+						file_put_contents($options['cookiefile'], $cookies);
+
+						if(!isset($options['returnheader']))
+							return $body;
+					}
+
+					$return = [];
+					$return['error_no'] = $err;
+					$return['error_msg'] = $errmsg;
+					
+					// http://php.net/manual/en/function.curl-getinfo.php#41332
+					$return['http_code'] = $http_code;
+					$return['headers'] = $header;
+					$return['body'] = $body;
+					$return['cookies'] = $cookies;
+					return $return;
 				}
-				else $cookies = false;
-				
-				if(isset($options['cookiefile']) && $cookies !== false){
-					file_put_contents($options['cookiefile'], $cookies);
-
-					if(!isset($options['returnheader']))
-						return $body;
-				}
-
-				$return = [];
-				$return['error_no'] = $err;
-				$return['error_msg'] = $errmsg;
-				
-				// http://php.net/manual/en/function.curl-getinfo.php#41332
-				$return['http_code'] = $http_code;
-				$return['headers'] = $header;
-				$return['body'] = $body;
-				$return['cookies'] = $cookies;
-				return $return;
 			}
+
+			curl_close($ch);
+			return $result;
+		};
+
+		# Execute CURL
+		if(is_array($url) === false){
+			$ch = curl_init($url);
+			curl_setopt_array($ch, $options_);
+			return $preprocess(curl_exec($ch), $ch);
 		}
-		
-		curl_close($ch);
-		return $result;
+		else {
+			$mh = curl_multi_init(); $ch = [];
+			for ($i=0, $n = count($url); $i < $n; $i++) { 
+				$ch[$i] = curl_init($url[$i]);
+				curl_setopt_array($ch[$i], $options_);
+				curl_multi_add_handle($mh, $ch[$i]); 
+			}
+			self::multiHandleCURL($mh, $ch, false, false);
+
+			$results = [];
+			foreach ($ch as &$value) {
+				$results[] = $preprocess(curl_multi_getcontent($value), $value);
+			}
+			return $results;
+		}
 	}
 
 	private static function parseHTTPHeader($data){
@@ -259,136 +281,118 @@ class WebRequest{
 		header('HTTP/1.0 500 Internal Server Error');
 		exit;
 	}
-	
+
 	// From other server to local file
-	public static function download($from, $to, $type = 'default'){
-		if($type === 'default'){
-			$file = file_get_contents($from, false, stream_context_create([
-    			'ssl'=>[
-    			    'verify_peer'=>false,
-    			    'verify_peer_name'=>false,
-    			],
-			]));
-			file_put_contents($to, $file);
-		}
-		
-		elseif($type ==='socket'){
-			#chunk = 10MB
-			$chunksize = 10 * (1024 * 1024);
-			
-			if(((string)get_http_response_code($from))!='200'){return false;}
-		
-			# parse_url breaks a part a URL into it's parts, i.e. host, path,
-			# query string, etc.
+	public static function download($path, $connection = 4, $onProgress = false){
+		$mh = curl_multi_init();
+		$options = [
+			CURLOPT_FOLLOWLOCATION => 1,
+			CURLOPT_SSL_VERIFYPEER => 0,
+			CURLOPT_SSL_VERIFYHOST => 0,
+			CURLOPT_RETURNTRANSFER => 0,
+			// CURLOPT_VERBOSE => 1,
+			CURLOPT_BINARYTRANSFER => 1,
+			CURLOPT_FRESH_CONNECT => 0,
+			CURLOPT_CONNECTTIMEOUT => 10,
+			CURLOPT_USERAGENT => self::$userAgent
+		];
 
-			$parts = parse_url($from);
-			$i_handle = fsockopen($parts['host'], 80, $errstr, $errcode, 5);
-			$o_handle = fopen($to, 'wb');
-		
-			if ($i_handle == false || $o_handle == false)
-				return false;
-		
-			if (!empty($parts['query']))
-				$parts['path'] .= '?' . $parts['query'];
-		
-			# Send the request to the server for the file
-	
-			$request = "GET {$parts['path']} HTTP/1.1\r\n";
-			$request .= "Host: {$parts['host']}\r\n";
-			$request .= "User-Agent: Mozilla/5.0\r\n";
-			$request .= "Keep-Alive: 115\r\n";
-			$request .= "Connection: keep-alive\r\n\r\n";
-			fwrite($i_handle, $request);
-		
-			# Now read the headers from the remote server. We'll need
-			# to get the content length.
-		
-			$headers = array();
-			while(!feof($i_handle)) {
-				$line = fgets($i_handle);
+		$addRequest = function(&$url, &$path) use(&$mh, &$options, &$connection) {
+			$size = self::contentSize($url);
+			$splits = range(0, $size, round($size / $connection));
+			$splitSize = count($splits);
 
-				if ($line == "\r\n")
-					break;
+			$parts = []; $fh = []; $ch = [];
+			for ($i = 0; $i < $splitSize; $i++) {
+			    $parts[$i] = $path."$i.tmp";
 
-				$headers[] = $line;
+			    $ch[$i] = curl_init($url);
+			    curl_setopt_array($ch[$i], $options);
+
+			    $fh[$i] = fopen($parts[$i], 'w+');
+			    curl_setopt($ch[$i], CURLOPT_FILE, $fh[$i]);
+
+			    $range = ($i === 0 ? 0 : $splits[$i] + 1).'-'.($i === $splitSize - 1 ? $size : $splits[$i + 1]);
+			    curl_setopt($ch[$i], CURLOPT_RANGE, $range);
+
+			    curl_multi_add_handle($mh, $ch[$i]); 
 			}
-		
-			# Look for the Content-Length header, and get the size
-			# of the remote file.
-		
-			$length = 0;
-			foreach($headers as $header) {
-				if (stripos($header, 'Content-Length:') === 0) {
-					$length = (int)str_replace('Content-Length: ', '', $header);
-					break;
+
+			return [[$parts, $fh, $path], $ch];
+		};
+
+		$reqs = []; $ch = [];
+		foreach ($path as $url => &$path) {
+			$tmp = $addRequest($url, $path);
+			$reqs[] = $tmp[0];
+			$ch[] = $tmp[1];
+		}
+
+		self::multiHandleCURL($mh, $ch, $onProgress);
+
+		$sizes = [];
+    	foreach ($reqs as &$req) {
+			$parts = &$req[0]; $fh = &$req[1]; $to = &$req[2];
+
+			$dest = fopen($to, "w+");
+			for ($i = 0, $n = count($parts); $i < $n; $i++) {
+				fseek($fh[$i], 0, SEEK_SET);
+
+				# Pipe stream
+				while (feof($fh[$i]) === false) {
+				    fwrite($dest, fread($fh[$i], 65535));
 				}
+				fclose($fh[$i]);
+			    unlink($parts[$i]);
 			}
-		
-			# Start reading in the remote file, and writing it to the
-			# local file one chunk at a time.
-		
-			$cnt = 0;
-			while(!feof($i_handle)) {
-				$buf = '';
-				$buf = fread($i_handle, $chunksize);
-				$bytes = fwrite($o_handle, $buf);
-				if ($bytes == false)
-					return false;
-				$cnt += $bytes;
-			
-				# We're done reading when we've reached the content length
-				if ($cnt >= $length) break;
-			}
-		
-			fclose($i_handle);
-			fclose($o_handle);
-			return $cnt;
-		}
-		
-		elseif($type === 'direct'){
-			$rh = fopen($from, 'rb');
-			$wh = fopen($to, 'wb');
-			if (!$rh || !$wh)
-				return false;
+			fclose($dest);
+			$sizes[] = filesize($to);
+    	}
 
-			while (!feof($rh)) {
-				if (fwrite($wh, fread($rh, 1024)) === FALSE)
-					return false;
-			}
-
-			fclose($rh);
-			fclose($wh);
-		}
-
-		elseif($type === 'curl'){
-			if(is_file($from))
-		        copy($from, $to); 
-		    else {
-		        $options = array(
-		        	CURLOPT_FILE    => fopen($to, 'w'),
-		        	CURLOPT_TIMEOUT =>  28800, // 8 hours
-		        	CURLOPT_URL     => $from
-		        );
-
-		        $ch = curl_init();
-		        curl_setopt_array($ch, $options);
-		        curl_exec($ch);
-		        curl_close($ch);
-		    }
-		}
-
-		else return false;
-		return true;
+		return $sizes;
 	}
-	
+
+	private static function multiHandleCURL(&$mh, &$chs, $onProgress, $closeCurl = true){
+		do {
+		    curl_multi_exec($mh, $running);
+		    curl_multi_select($mh);
+
+		    if($onProgress !== false){
+    			$info = curl_multi_info_read($mh);
+    			if($info !== false) continue;
+
+    			foreach ($chs as &$ch) {
+			    	$i = array_search($info['handle'], $ch);
+			    	if($i === -1) continue;
+
+			    	$k = array_search($ch, $chs);
+
+			    	$info = curl_getinfo($info['handle']);
+			    	$onProgress($k, $i, $info['size_download'], $info['download_content_length'], $info['speed_download']);
+			    	break;
+    			}
+		    }
+		} while ($running > 0);
+
+		# Close handles
+		foreach ($chs as &$ch) {
+			foreach ($ch as &$value) {
+				curl_multi_remove_handle($mh, $value);
+
+				if($closeCurl === true)
+					curl_close($value);
+			}
+		}
+		curl_multi_close($mh);
+	}
+
 	// allowedExt = array
 	// From client browser to this server
-	public static function receiveFile($directory, $allowedExt, $rename = ''){
-		if(!empty($_FILES)) 
-		{
-			$file = &$_FILES['Filedata'];
+	public static function receiveFile($field, $directory, $allowedExt, $rename = ''){
+		if(!empty($_FILES)){
+			$file = &$_FILES[$field];
 			$targetFile = realpath($directory).'/'. ($rename !== '' ? $rename : $file['name']);
-			
+
 			// Validate the filetype
 			$extension = strtolower(pathinfo($file['name'])['extension']);
 			if(in_array($extension, $allowedExt)){
@@ -396,11 +400,17 @@ class WebRequest{
 				move_uploaded_file($file['tmp_name'], $targetFile);
 				return true;
 			} 
-			
+
 			// The file type wasn't allowed
-			else 
-				return false;
+			else return false;
 		}
 		return false;
+	}
+
+	public static function contentSize($url){
+		$data = self::loadURL($url, ['method'=>'HEAD', 'returnheader'=>true]);
+		if(isset($data['headers']['Content-Length']) === false)
+			return 0;
+		return (float)$data['headers']['Content-Length'];
 	}
 }
